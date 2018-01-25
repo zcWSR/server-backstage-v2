ecbe9fbcce733e6fe47f8cccaf5721e0e51760d9
congzz.zhao <congzz.zhao@qunar.com>
2018-01-25 20:43:53 +0800

    dev

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const mainRoute = require('./routes/mainRoute');
+const blogRoute = require('./routes/blogRoute');
+const konachanRoute = require('./routes/konaChanRoute');
 
-app.use('/', require('./routes/mainRoute'));
-app.use('/blog', require('./routes/blogRoute'));
-app.use('/konachan', require('./routes/konaChanRoute'));
+app.use('/', mainRoute);
+app.use('/blog', blogRoute);
+app.use('/konachan', konachanRoute);
 
 module.exports = app;
