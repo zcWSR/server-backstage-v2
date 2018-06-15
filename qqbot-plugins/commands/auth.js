b000359c774a7ae2b1261951635119d9724e70b3
zcWSR <zhaocong282@sina.com>
2018-06-16 00:29:47 +0800

    Fix: 异步处理

--- qqbot-plugins/commands/auth.js
+++ qqbot-plugins/commands/auth.js
@@ -5,6 +5,6 @@ export const info = `查看你的权限, '!auth'来调用`;
 
 export async function exec (params, body) {
   const { group_id, sender_id } = body;
-  const role = BotService.getSenderRole(group_id, sender_id);
+  const role = await BotService.getSenderRole(group_id, sender_id);
   BotService.sendGroup(group_id, `你的权限名为: '${role}'`);
 }
