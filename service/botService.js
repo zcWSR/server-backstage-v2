801da0ef80740161dcc004342a73720667dd39d0
zcWSR <zhaocong282@sina.com>
2018-06-16 00:33:39 +0800

    Fix: 修复权限名称

--- service/botService.js
+++ service/botService.js
@@ -16,12 +16,12 @@ export async function isSenderOwner(group_id, sender_id) {
 }
 
 export async function isSenderAdmin(group_id, sender_id) {
-  return await getSenderRole(group_id, sender_id) === 'attend';
+  return await getSenderRole(group_id, sender_id) === 'admin';
 }
 
 export async function isSenderOwnerOrAdmin(group_id, sender_id) {
   const role = await getSenderRole(group_id, sender_id);
-  return role === 'attend' || role === 'owner';
+  return role === 'admin' || role === 'owner';
 }
 
 export async function getSenderRole(group_id, sender_id) {
