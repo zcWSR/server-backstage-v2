807706546b925c10b3edd015de947d16ec419dc1
zcWSR <zhaocong282@sina.com>
2018-06-22 01:50:08 +0800

    Fix: axios post 参数错误

--- service/botService.js
+++ service/botService.js
@@ -19,14 +19,24 @@ export async function loadGroupConfig() {
 }
 
 export async function clearGroupConfig(group_id) {
-  await db('qqbot').update('config', JSON.stringify({})).where('group_id', group_id);
+  await db('qqbot')
+    .update('config', JSON.stringify({}))
+    .where('group_id', group_id);
 }
 
 export async function saveGroupConfig(group_id, config) {
-  const configBefore = await db('qqbot').first('group_id').where('group_id', group_id);
-  logger.debug(group_id, '群插件配置before:', JSON.stringify(configBefore, null, 2));
+  const configBefore = await db('qqbot')
+    .first('group_id')
+    .where('group_id', group_id);
+  logger.debug(
+    group_id,
+    '群插件配置before:',
+    JSON.stringify(configBefore, null, 2)
+  );
   if (configBefore) {
-    await db('qqbot').update('config', config).where('group_id', group_id);
+    await db('qqbot')
+      .update('config', config)
+      .where('group_id', group_id);
   } else {
     logger.debug(group_id, '群插件配置不存在, 创建之');
     await db('qqbot').insert({ [group_id]: config });
@@ -39,17 +49,15 @@ export function sayAgain(group_id, content, timeout = 2000) {
   }, timeout);
 }
 export function sendGroup(group_id, message) {
-  axios.post('http://localhost:5000/send_group_message', {
-    data: { group_id, message }
-  });
+  axios.post('http://localhost:5000/send_group_message', { group_id, message });
 }
 
 export async function isSenderOwner(group_id, user_id) {
-  return await getSenderRole(group_id, user_id) === 'owner';
+  return (await getSenderRole(group_id, user_id)) === 'owner';
 }
 
 export async function isSenderAdmin(group_id, user_id) {
-  return await getSenderRole(group_id, user_id) === 'admin';
+  return (await getSenderRole(group_id, user_id)) === 'admin';
 }
 
 export async function isSenderOwnerOrAdmin(group_id, user_id) {
@@ -60,9 +68,10 @@ export async function isSenderOwnerOrAdmin(group_id, user_id) {
 export async function getSenderRole(group_id, user_id) {
   try {
     // await axios.get('http://localhost:5000/openqq/get_group_info');
-    const meta = await axios.post('http://localhost:5000/get_group_member_info', {
-      data: { group_id, user_id }
-    });
+    const meta = await axios.post(
+      'http://localhost:5000/get_group_member_info',
+      { group_id, user_id }
+    );
     let memberInfo = meta.data;
     if (!memberInfo.user_id) return null;
     return memberInfo.role;
