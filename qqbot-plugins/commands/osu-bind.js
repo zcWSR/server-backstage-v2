275c5f2e6ebbc8f23501f3c9a1fa7a3f3f47a20c
zcWSR <zhaocong282@sina.com>
2018-07-04 15:46:22 +0800

    Fix: 修复全角逗号不能被识别的bug

--- qqbot-plugins/commands/osu-bind.js
+++ qqbot-plugins/commands/osu-bind.js
@@ -12,6 +12,7 @@ export async function exec(params, body) {
     BotService.sendGroup(group_id, `非法调用, 使用'!help bind'查看调用方式`);
     return;
   }
+  params.replace('，', ','); // 处理全角逗号
   params = params.split(',');
   let osuName = params[0];
   let mode = params[1] ? parseInt(params[1]) || 0 : 0;
