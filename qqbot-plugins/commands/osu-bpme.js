5b648a5bad649677ab9dd51761f57b0daee33803
zcWSR <zhaocong282@sina.com>
2018-07-06 18:28:06 +0800

    Feat: info文案

--- qqbot-plugins/commands/osu-bpme.js
+++ qqbot-plugins/commands/osu-bpme.js
@@ -19,7 +19,7 @@ export async function exec(params, body) {
   }
   const bindUserInfo = await OSUService.getBindedInfo(group_id, user_id);
   if (!bindUserInfo) {
-    BotService.sendGroup(group_id, `非法参数, 您未绑定osu!账号, 使用'!bind xxx'进行账号绑定`);
+    BotService.sendGroup(group_id, `非法参数, 您未绑定osu!账号, 使用'!bind'进行账号绑定`);
     return;
   }
   const info = await OSUService.getBP(bindUserInfo, index);
